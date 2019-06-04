7ee634d8680ed00ce0555d0c155dce3803586bd2
Fabian Balensiefer <balensiefer@uni-bonn.de>
2019-06-04 22:53:55 +0200

    update table 4

--- spyder_ide.py
+++ spyder_ide.py
@@ -54,18 +54,19 @@ df = pd.read_stata('data/mergersample_controls.dta')
 index = ['popdensity','poptot','medincome','pminority','pcollege','pmortgage','pincome','cont_totalbranches', 'cont_brgrowth','cont_NumSBL_Rev1','cont_total_origin']
 df_t = pd.DataFrame(columns=['Variable', 'Exposed', 'All other', 'p-value 01', 'Control', 'p-value 02'], index=index)
 df.drop_duplicates(keep='first', inplace=True)
-df_exposed = df.loc[df.overlap==1] 
+df_exposed = df.loc[df.overlap==1]
 df_exposed = df_exposed.filter(regex=filter_cols).T
 df = pd.read_stata('data/mergersample_controls.dta')
 df.drop_duplicates(keep='first', inplace=True)
-df_all     = df.loc[df.overlap==0] 
+df_all     = df.loc[df.overlap==0]
 df_all     = df_all.filter(regex=filter_cols).T
 df01 = pd.read_stata('data/replication_input.dta')
 df01 = df01.filter(regex='state_fps|cnty_fps|tractstring|overlap|mergerID')
 df01.drop_duplicates(keep='first', inplace=True)
 df02 = pd.read_stata('data/mergersample_controls.dta')
 df02.drop_duplicates(keep='first', inplace=True)
-df = pd.merge(df01,df02,  on=['state_fps','cnty_fps','tractstring','mergerID'])
+#df = pd.merge(df01,df02,  on=['state_fps','cnty_fps','tractstring','mergerID'], how='inner', suffixes=('', '_y'))
+df = pd.merge(df01,df02,  on=['state_fps','cnty_fps','tractstring','mergerID'], how='inner')
 df = df.loc[df.overlap_y==0]
 #f_control = df.groupby(['state_fps','cnty_fps'], as_index=True)
 df_control = df.groupby('state_fps')
@@ -73,8 +74,8 @@ df_control = df_control.agg(np.mean)
 df_control = df_control.filter(regex=filter_cols).T
 df = df.filter(regex=filter_cols)
 df_t['Variable']  = list(df)
-df_t['Exposed']   = np.round(np.nanmean(df_exposed, axis=1), 3) 
-df_t['All other'] = np.round(np.nanmean(df_all, axis=1), 3) 
+df_t['Exposed']   = np.round(np.nanmean(df_exposed, axis=1), 3)
+df_t['All other'] = np.round(np.nanmean(df_all, axis=1), 3)
 ptemp = stats.ttest_ind(df_exposed, df_all, axis=1, equal_var=True, nan_policy='omit')
 df_t['p-value 01']   = np.round(np.ma.getdata(ptemp[1]), 3)
 df_t['Control']   = np.round(np.nanmean(df_control, axis=1), 3)
@@ -93,17 +94,17 @@ print(df_t.to_string(index=False))
 filter_cols = 'poptot|popdensity|pminority|pcollege|pincome|medincome|pmortgage|cont_totalbranches|cont_brgrowth|cont_total_origin|cont_NumSBL_Rev1'
 df = pd.read_stata('data/alltract_controls.dta')
 #index = ['popdensity','poptot','medincome','pminority','pcollege','pmortgage','pincome','cont_totalbranches', 'cont_brgrowth','cont_NumSBL_Rev1','cont_total_origin']
-index = ['popdensity', 'poptot', 'medincome', 'pminority', 'pcollege', 'pmortgage', 'totalbranches', 'brgrowth', 'NumSBL_Rev1', 'total_origin', 'pincome', 'num_closings']
+index = ['popdensity', 'poptot', 'medincome', 'pminority', 'pcollege', 'pmortgage', 'totalbranches', 'brgrowth', 'NumSBL_Rev1', 'total_origin', 'pincome']
 df_t = pd.DataFrame(columns=['Variable', 'All', 'Closings', 'Merger'], index=index)
 df.drop_duplicates(keep='first', inplace=True)
 
 df_all = df.loc[df['year']>=2002]
 df_all = df_all.loc[df_all['year']<=2007]
 df_all = df_all.groupby(['state_fps', 'cnty_fps', 'tractstring'])
-df_all = df_all.agg(np.max)
+df_all = df_all.agg(np.nanmax)
 df_all = df_all.loc[df_all['totalbranches']>0]
-df_all = df_all.agg(np.mean)
-alltract_index = 'popdensity|poptot|medincome|pminority|pcollege|pmortgage|totalbranches|brgrowth|NumSBL_Rev1|total_origin|pincome|num_closings'
+df_all = df_all.agg(np.nanmean)
+alltract_index = 'popdensity|poptot|medincome|pminority|pcollege|pmortgage|totalbranches|brgrowth|NumSBL_Rev1|total_origin|pincome'
 df_all = df_all.filter(regex=alltract_index)
 
 df_closing = df.loc[df['year']>=2002]
@@ -120,14 +121,13 @@ df_merger = df.loc[df['year']>=2002]
 df_merger = df_merger.loc[df_merger['year']<=2007]
 df01 = pd.read_stata('data/alltract_controls.dta')
 df01.drop_duplicates(keep='first', inplace=True)
-df_merger = pd.merge(df_merger,df01,  on=['state_fps','cnty_fps','tractstring'])
+df_merger = pd.merge(df_merger,df01,  on=['state_fps','cnty_fps','tractstring'], how='inner', suffixes=('', '_y'))
 df_merger = df_merger.groupby(['state_fps', 'cnty_fps', 'tractstring'])
 df_merger = df_merger.agg(np.nanmax)
 df_merger = df_merger.agg(np.nanmean)
-s = pd.Series(filter_cols)
-snew = pd.Series([...])
-df_merger.set_index([s, snew])
-df_merger = df_merger.filter(regex=filter_cols).T
+df_merger.rename(index= {'cont_totalbranches': 'totalbranches', 'cont_brgrowth':'brgrowth', 'cont_NumSBL_Rev1':'NumSBL_Rev1','cont_total_origin':'total_origin'},  inplace = True)
+df_merger = df_merger.filter(regex=alltract_index).T
+df_merger = df_merger.iloc[4:15]
 
 
 df_t['Variable']  = list(index)
